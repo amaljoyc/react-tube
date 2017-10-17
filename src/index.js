3386316a238a653a3956fede3d5f459673a617f9
Amal Joy Chemparathy <amaljoyc@gmail.com>
2017-10-18 01:51:32 +0200

    video-list

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 import env from './env'
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
 const API_KEY = process.env.GOOGLE_API_KEY || '';
 
-YTSearch({key: API_KEY, term: 'cricket'}, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      Hello World!
-      <SearchBar />
-    </div>
-  );
-};
+    this.state = {videos : []};
+
+    YTSearch({key: API_KEY, term: 'cricket'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World!
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
