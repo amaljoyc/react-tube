c247bf61be71909283738775e3cbbc240c062faf
Amal Joy Chemparathy <amaljoyc@gmail.com>
2017-10-18 01:22:20 +0200

    env var

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+import env from './env'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar';
 
-const API_KEY = '';
+const API_KEY = process.env.GOOGLE_API_KEY || '';
+
+YTSearch({key: API_KEY, term: 'cricket'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
